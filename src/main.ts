d5291ca570602b2f9457fafda35c1cf450e7f8fc
Juliot Nuñez <nunezjuliot@gmail.com>
2025-01-30 03:28:15 -0400

    Removes unnecessary import statement
    Removes an unused import statement in the main.ts file.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from 'pinia'
-
 import { IonicVue } from "@ionic/vue";
 
 /* Core CSS required for Ionic components to work properly */
